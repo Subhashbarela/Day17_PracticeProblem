730b26f39622f4adcef854e9cfb7151595140c73
Subhash Barela <barelasubhash16@gmail.com>
2023-05-26 18:38:22 +0530

    initial commit

---
+++ Day17_IndianSelciusData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17_IndianSelciusData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indian Selcius Data Program");
+        }
+    }
+}
